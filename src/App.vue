cb30b16b56434124891fd662d04483565d862b91
xanqus <sbk98031816@gmail.com>
2021-06-25 16:25:48 +0900

    vite로 프로젝트 생성 및 필요없는 파일 제거

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsa explicabo quasi
+  voluptate! Ratione, magni tempora, et reiciendis perferendis voluptatibus
+  ducimus ut quia nemo, harum minima voluptates deserunt enim ab quod!
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default defineComponent({
+  name: "App",
+});
+</script>
+
+<style>
+html,
+body {
+  margin: 0;
+}
+
+#app {
+  font-family: Avenir, Arial, Helvetica, sans-serif;
+  -webkit-font-smoothing-: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
